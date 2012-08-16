53771bb5febd29d34a951264fa4b542e375925b8
armab <blog@arma.biz>
2012-08-16 06:00:08 +0300

    Session already start (headers already sent) fix
    Partially fix for error when login:
    Warning: session_start() [function.session-start]: Cannot send session cache limiter - headers already sent

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
+<?php session_start(); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <?php
-session_start();
 include ('config.php');
 
 mysql_connect($hostname, $username, $password) or die (mysql_error());
